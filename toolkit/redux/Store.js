c61803d41945ba2e3f5614a1fe2a87f030d780f1
Gajanan255 <147083047+Gajanan255@users.noreply.github.com>
2023-10-19 13:20:04 +0530

    Delete toolkit directory
    some issue

--- toolkit/redux/Store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import cardReducer from "./Cardslice";
-const store=configureStore({
-    reducer:{
-        card:cardReducer,
-    }
-})
-
-export default store;   
