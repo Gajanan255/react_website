0386c23616a7d4e1c71de5fa5ef4260c3aa43319
Gajanan255 <147083047+Gajanan255@users.noreply.github.com>
2023-10-19 13:25:52 +0530

    Shopping App Using Redux-toolkit
    In this  react-redux tool-kit project are fetch the product from fake-Api site are how to work in addToCart button click that's item store in list page and in this page remove button how to work and how to use CreateSlice & ConfigureStore & actionCreaters & createAction & useDispatch & use Selector and anchor tag Through how to different click open in different social media are open depends on you put a link inside the anchor tag and BrowserRouter , Routes , Route through one page to another page how to go without server Refresh and contact detail's create a table form  and there are  create 6 pages => 1. Home page & 2. Aboutas page & 3.Product page & 4. Contact page  & 5. List page and another Productslist page create and this Productslist page are implement in Product Component page how to work are all explained type of in this real project of my react-redux project with using React js and Redux tool-kit and HTML  and CSS and JavaScript I hope you see the project.

---
+++ Shopping/Pagesss/Products.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+import Productlist from './Productlist';
+import { useSelector } from 'react-redux';
+function Products() {
+  const items = useSelector((state) => state.card)
+  return (
+    <div>
+      <div style={{backgroundColor:"aliceblue",width:"100%",height:"100%"}}>
+        <nav>
+          <input type="checkbox" id="check" />
+          <label for="check" className="checkbtn">
+            <h1>L</h1>
+          </label>
+          <label className="logo">
+            <Link to="/" style={{ color: "white" }}> Shop Now</Link></label>
+          <ul>
+            <li><Link className="active" to="/"> Home</Link></li>
+            <li><Link to="/product" className="active">Product</Link></li>
+            <li><Link to="/about" className="active">About</Link></li>
+            <li><Link to="/contact" className="active">Contact</Link></li>
+            <li><Link to="/list" className="active">List</Link></li>
+          </ul>
+        </nav>
+        <h1 style={{ textAlign: "center",color:"black" }}>Card</h1>
+
+        <span style={{ color: "white", fontSize: "20PX", float: "right", backgroundColor: "black", marginTop: "-40px" }}>items: {items.length}</span>
+        <Productlist />
+      </div>
+      <div className='footerr'>
+        <p>Created By | <a href='#' className='copy'>Gajanan</a></p>
+      </div>{/*complite footer*/}
+    </div>
+  )
+}
+
+export default Products
